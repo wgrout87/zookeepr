54dd0b9400f3dfe82844e58d05113fb1280ca297
Will G <wgrout87@gmail.com>
2022-05-17 20:31:48 -0600

    front end

--- server.js
+++ server.js
@@ -15,6 +15,8 @@ app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
 
+app.use(express.static("public"));
+
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
     // Note that we save the animalsArray as filteredResults here:
@@ -106,13 +108,29 @@ app.get("/api/animals/:id", (req, res) => {
 app.post("/api/animals", (req, res) => {
     req.body.id = animals.length.toString();
 
-    if(!validateAnimal(req.body)) {
+    if (!validateAnimal(req.body)) {
         res.status(400).send("The animal is not properly formatted.");
     } else {
         const animal = createNewAnimal(req.body, animals);
-    
+
         res.json(animal);
     }
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get("/animals", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/animals.html"));
+});
+
+app.get("/zookeepers", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/zookeepers.html"));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.listen(PORT, () => {
